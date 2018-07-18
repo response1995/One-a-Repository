967165cf72c4e581566947f517c165817aceaa4a
response <c784898478@163.com>
2018-07-18 14:52:57 +0800

    WebService

---
+++ src/main/java/com/offcn/Service.java
@@ -0,0 +1,20 @@
+package com.offcn;
+
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+@WebService
+public class Service {
+
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:9001/Service/hello", new Service());
+		System.out.println("服务器发布成功");
+	}
+	
+	public String getValue(String name){
+		return "I'm name is " + name;
+	}
+	
+	
+	
+}
